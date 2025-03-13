65626b7c671427a451d99183f2ebfb32f9fc0ca1
Visnja Mladenovic <visnjagithub@gmail.com>
2025-03-13 14:13:38 +0100

    initial commit

---
+++ Project-Nirvana/src/main/kotlin/com/snjavi/projectnirvana/ProjectNirvanaApplication.kt
@@ -0,0 +1,11 @@
+package com.snjavi.projectnirvana
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectNirvanaApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectNirvanaApplication>(*args)
+}
---
+++ Project-Nirvana/src/test/kotlin/com/snjavi/projectnirvana/ProjectNirvanaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.snjavi.projectnirvana
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectNirvanaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
